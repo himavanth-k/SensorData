4298211153472cc9db1b98e64cb9607b8d77e126
Himavanth Kandula <84218737+himavanth-k@users.noreply.github.com>
2023-09-28 01:16:30 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -20,9 +20,7 @@ pir_sensor = 11
 piezo = 7
 
 GPIO.setmode(GPIO.BOARD)
-
 GPIO.setup(piezo, GPIO.OUT)
-
 GPIO.setup(pir_sensor, GPIO.IN)
 
 current_state = 0 
@@ -31,16 +29,18 @@ try:
     while True:
         time.sleep(0.1)
         current_state = GPIO.input(pir_sensor)
+        
         if current_state == 1:
-            print("GPIO pin %s is %s" % (pir_sensor, current_state))
+            print("Motion detected")
             GPIO.output(piezo, True)
             time.sleep(1)
             GPIO.output(piezo, False)
-            time.sleep(5)
+            
             # Push data to Firebase
             db.child("motion").push({"status": "detected"})
         else:
-            print("GPIO pin %s is %s" % (pir_sensor, current_state))
+            print("No motion detected")
+            
             # Push data to Firebase
             db.child("motion").push({"status": "not detected"})
 
