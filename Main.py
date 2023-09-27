a779cef23bc59ed11af08b6aaa314e4342d8301f
Himavanth Kandula <84218737+himavanth-k@users.noreply.github.com>
2023-09-28 01:12:55 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,50 @@
+import RPi.GPIO as GPIO
+import time
+from pyrebase import initialize_app
+
+# Initialize Firebase
+config = {
+    "apiKey": "YOUR_API_KEY",
+    "authDomain": "YOUR_AUTH_DOMAIN",
+    "databaseURL": "YOUR_DATABASE_URL",
+    "projectId": "YOUR_PROJECT_ID",
+    "storageBucket": "YOUR_STORAGE_BUCKET",
+    "messagingSenderId": "YOUR_MESSAGING_SENDER_ID",
+    "appId": "YOUR_APP_ID"
+}
+
+firebase = initialize_app(config)
+db = firebase.database()
+
+pir_sensor = 11
+piezo = 7
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(piezo, GPIO.OUT)
+
+GPIO.setup(pir_sensor, GPIO.IN)
+
+current_state = 0 
+
+try:
+    while True:
+        time.sleep(0.1)
+        current_state = GPIO.input(pir_sensor)
+        if current_state == 1:
+            print("GPIO pin %s is %s" % (pir_sensor, current_state))
+            GPIO.output(piezo, True)
+            time.sleep(1)
+            GPIO.output(piezo, False)
+            time.sleep(5)
+            # Push data to Firebase
+            db.child("motion").push({"status": "detected"})
+        else:
+            print("GPIO pin %s is %s" % (pir_sensor, current_state))
+            # Push data to Firebase
+            db.child("motion").push({"status": "not detected"})
+
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
